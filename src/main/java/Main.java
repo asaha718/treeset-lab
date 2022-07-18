ee619346a729c1f4f91f92ad558bb134a0881ca9
Anug Saha <anugsaha@gmail.com>
2022-07-18 13:08:31 -0400

    convert Array to List and then pass in to new TreeSet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 1, 4, 5, 2, 34, 36, 5, 4, 23, 45, 6, 8, 56, 34, 37 };
-        ArrayList<Integer> res = getNums(numbers);
+        Integer[] numbers = { 1, 4, 5, 2, 34, 36, 5, 4, 23, 45, 6, 8, 56, 34, 37 };
+        TreeSet<Integer> res = getNums(numbers);
         System.out.println(res); // [34, 36, 37, 45, 56]
     }
 
-    public static ArrayList<Integer> getNums(int[] nums) {
-        // your code here
-        return null;
+    public static TreeSet<Integer> getNums(Integer[] nums) {
+        List<Integer> listOfNums = Arrays.asList(nums);
+        TreeSet<Integer> treeSet = new TreeSet<Integer>(listOfNums);
+        return (TreeSet<Integer>) treeSet.tailSet(25);
     }
 }
